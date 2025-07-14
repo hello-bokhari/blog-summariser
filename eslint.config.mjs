f09ca7dce92a21bdd8b430e2a5b9f24644862e82
Syed Abbad Hasan Bokhari <abbadhasan750@gmail.com>
2025-07-14 23:32:55 +0500

    Disable ESLint rules for no-explicit-any and no-unused-vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    // Apply overrides globally
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
+
